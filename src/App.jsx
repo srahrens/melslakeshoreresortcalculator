9c3c4a2bebfb3ebefbef37badd7e18910d9779cf
Summer Ahrens <srahrens@wisc.edu>
2026-04-02 12:10:53 -0500

    Got rid of built in css and got the google sheet data pulled in

--- src/App.jsx
+++ src/App.jsx
@@ -1,120 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useState, useEffect } from 'react'
+import {Card, Container, Button, Row, Col} from 'react-bootstrap';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [sheetData, setSheetData] = useState([]); // All data from google sheet
+  const [departments, setDepartments] = useState([]); // All departments listed in sheet
+  const [currentTab, setCurrentTab] = useState(); // The current tab the user is on
+
+  useEffect(() => {
+        getItemList();
+    }, [])
+
+  function getItemList() {
+      fetch("https://opensheet.elk.sh/1_hdFkBTCwqWiRa8Tkx2huEamIMqg5bRjTCOYV30xK1s/1")
+      .then(res => res.json())
+      .then(data => {
+          setSheetData(data);
+          let uniqueDepartments = data.filter((obj, index, self) =>
+            index === self.findIndex((t) => t.Department === obj.Department)
+          );
+          setDepartments(uniqueDepartments.map(item => item.Department));
+      })
+  }
+  
+
+  console.log(departments);
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    <Container fluid>
+      <Row>
+        <Col lg={3} style={{backgroundColor: "blue"}}>
+        </Col>
+        <Col lg={2} style={{backgroundColor: "red"}}>
+        </Col>
+          <Col md={6} lg={7}>
+              <Container fluid>
+                  <Row>
+                      {
+                          sheetData.map(c => <Col key={c.Name} lg={6}>
+                              <Card><Card.Title>{c.Name}</Card.Title></Card>
+                          </Col>)
+                      }
+                  </Row>
+              </Container>
+          </Col>
+      </Row>
+  </Container>
   )
 }
 
